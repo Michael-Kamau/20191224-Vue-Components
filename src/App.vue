924ad5fca05e15bc5219e71e4506a6d87a9c096e
Michael-Kamau <kamau.karitu@gmail.com>
2019-12-27 11:03:53 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+<!--    <img alt="Vue logo" src="./assets/logo.png">-->
+<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+      <Index v-bind:products="products" v-on:view-product="viewProduct"/>
+  </div>
+</template>
+
+<script>
+import Index from './components/Index'
+
+export default {
+  name: 'app',
+  components: {
+    Index
+  },
+    data(){
+      return{
+          products: [
+              {id:1, name: 'Mazda', amount: 150000},
+              {id:2, name: 'Toyota', amount: 120000},
+              {id:3, name: 'Nissan', amount: 180000},
+              {id:4, name: 'Subaru', amount: 220000},
+          ],
+      }
+    },
+    methods: {
+        viewProduct: function (id) {
+            this.products=this.products.filter(product => product.id == id);
+
+        }
+    },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+  margin-top: 60px;
+    line-spacing: 1;
+}
+</style>
---
+++ src/components/Index.vue
@@ -0,0 +1,82 @@
+<template>
+    <!-- Your HTML goes here -->
+    <div class="home">
+        <h1>Welcome</h1>
+        <div>
+            <table>
+                <thead>
+                <tr>
+                    <th>Product</th>
+                    <th>Amount</th>
+                    <th>Action</th>
+                </tr>
+                </thead>
+                <tr v-for="product in products" :key="product.id">
+                    <td>{{product.name}}</td>
+                    <td>{{product.amount}}</td>
+                    <td><CustomButton v-bind:product="product.id" v-on:view-product="$emit('view-product',product.id)">
+                        <span>
+                            View {{product.name}}
+                        </span>
+                    </CustomButton></td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</template>
+
+<script>
+    import CustomButton from './CustomButton'
+
+    export default {
+        name: 'Index',
+        active: true,
+        props:["products"],
+        components: {
+            CustomButton
+        },
+        data() {
+            return {
+                amount: 1000,
+
+                classObject: {
+                    active: true,
+                    'text-danger': false
+                },
+                myAlert: function () {
+                    alert("Are you sure you want to submit the form")
+
+                }
+            }
+        },
+
+    }
+
+
+</script>
+
+<style>
+
+    .active {
+        color: blue;
+    }
+
+    label{
+        padding:10px;
+    }
+
+    table, td, th {
+        border: 1px solid #ddd;
+        text-align: left;
+    }
+
+    table {
+        border-collapse: collapse;
+        width: 50%;
+    }
+
+    th, td {
+        padding: 15px;
+    }
+
+</style>
